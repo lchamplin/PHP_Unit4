fddc2215aad9879f0249479c510088e5b69fe7b8
Lauren Champlin <lchamplin@mymail.mines.edu>
2021-11-28 14:21:19 -0700

    part 2

--- Unit4_adminProduct.php
+++ Unit4_adminProduct.php
@@ -12,7 +12,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body>
-<div class="row">
+<div class="wrapper">
   <div class="column1">
           <?php
         $conn = getConnection();
