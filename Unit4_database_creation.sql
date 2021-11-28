816577fb8b6d596be52d5d16e7bdee30ec5ae289
Lauren Champlin <lchamplin@mymail.mines.edu>
2021-11-28 12:23:12 -0700

    part 1

--- Unit4_database_creation.sql
+++ Unit4_database_creation.sql
@@ -18,6 +18,7 @@ CREATE TABLE Product (
     image_name varchar(255), 
     price decimal(6, 2), 
     in_stock int,
+    inactive boolean DEFAULT 0,
     PRIMARY KEY (id)
 );
 
@@ -37,8 +38,8 @@ CREATE TABLE Orders (
 INSERT INTO Customer (first_name, last_name, email) VALUES('Mickey', 'Mouse', 'mmouse@mines.edu');
 INSERT INTO Customer (first_name, last_name, email) VALUES('Jordan', 'Ratliff', 'cwiggl3s@gmail.com');
 
-INSERT INTO Product (product_name, image_name, price, in_stock) VALUES('Gummy Bears', 'gummy_bears.jpg', 5, 0);
-INSERT INTO Product (product_name, image_name, price, in_stock) VALUES('Chocolates', 'chocolates.jpg', 3, 3);
-INSERT INTO Product (product_name, image_name, price, in_stock) VALUES('Caramels', 'caramels.jpg', 8, 32);
+INSERT INTO Product (product_name, image_name, price, in_stock, inactive) VALUES('Gummy Bears', 'gummy_bears.jpg', 5, 0, 1);
+INSERT INTO Product (product_name, image_name, price, in_stock, inactive) VALUES('Chocolates', 'chocolates.jpg', 3, 3, 0);
+INSERT INTO Product (product_name, image_name, price, in_stock, inactive) VALUES('Caramels', 'caramels.jpg', 8, 32, 0);
 
 
