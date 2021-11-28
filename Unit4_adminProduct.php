21dea39cc15230e6e576271e19b9ad3bdb2c52be
Lauren Champlin <lchamplin@mymail.mines.edu>
2021-11-28 14:00:51 -0700

    part 2

--- Unit4_adminProduct.php
+++ Unit4_adminProduct.php
@@ -20,7 +20,7 @@
         ?>
   </div>
 
-  <div class="column">
+  <!-- <div class="column">
 <form>
    <fieldset class="product">
     <legend>Product Info</legend>
@@ -36,7 +36,7 @@
                         <button id="update">UPDATE<button>
                         <button id="delete">DELETE<button>
                 </span>
-  </div>
+  </div> -->
 </div> 
 </body>
 </html>
