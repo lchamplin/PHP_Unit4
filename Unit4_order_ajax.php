367431ad5ceff047d4f4cd0f9a92455192e9bbb8
Lauren Champlin <lchamplin@mymail.mines.edu>
2021-11-29 19:00:59 -0700

    part 3 update

--- Unit4_order_ajax.php
+++ Unit4_order_ajax.php
@@ -17,19 +17,19 @@ if($action=="add"){
         addProduct($conn, $name, $image, $price, $quantity, $inactive);
         echo getProductTableHTML($conn);
 }
-// else if($action=="update"){
-//         $id=(int)$_POST['id'];
-//         updateProductById($conn, $id, $name, $image, $price, $quantity, $inactive)
-//         echo getProductTableHTML($conn);
-// }
+if($action=="update"){
+        $id=(int)$_POST['id'];
+        updateProductById($conn, $id, $name, $image, $price, $quantity, $inactive)
+        echo getProductTableHTML($conn);
+}
 // else if($action=="delete"){
 //         $id=(int)$_POST['id'];
 //         deleteProductById($conn, $id);
 //         echo getProductTableHTML($conn);
 
 // }
-else{
-        echo "Error";
-}
+
+echo "Error";
+
 
 ?>
